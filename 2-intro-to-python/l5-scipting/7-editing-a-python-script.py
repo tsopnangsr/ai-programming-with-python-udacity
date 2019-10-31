109686c5bc09e499b6498432d3de8b298380dab7
Romaric Tsopnang <tsopnangsr@gmail.com>
2019-10-31 11:04:38 +0100

    adding eval function description

---
+++ 2-intro-to-python/l5-scipting/7-editing-a-python-script.py
@@ -0,0 +1,23 @@
+#7-editing-a-python-script.py
+
+
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string * how_many_snakes)
+
+#eval() is a function that evaluate a string line a line in python
+
+
