2447ca251157a166b9b63675c762b80fc675142e
Romaric Tsopnang <tsopnangsr@gmail.com>
2019-10-31 11:27:37 +0100

    9-Student-missing-assignments-solution

--- 2-intro-to-python/l5-scipting/9-students-missing-assignments.py
+++ 2-intro-to-python/l5-scipting/9-students-missing-assignments.py
@@ -19,3 +19,18 @@ for i in range(len(names)):
 
 # write a for loop that iterates through each set of names, assignments, and grades to print each student's message
 
+#Solution
+"""
+
+names = input("Enter names separated by commas: ").title().split(",")
+assignments = input("Enter assignment counts separated by commas: ").split(",")
+grades = input("Enter grades separated by commas: ").split(",")
+
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+submit before you can graduate. You're current grade is {} and can increase \
+to {} if you submit all assignments before the due date.\n\n"
+
+for name, assignment, grade in zip(names, assignments, grades):
+    print(message.format(name, assignment, grade, int(grade) + int(assignment)*2))
+
+"""
